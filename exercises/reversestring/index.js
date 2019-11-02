688097fdaec62db0f784d5f19b2994165dc161f9
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-01 17:19:19 -0700

    Solve question (my way)

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// ------------------------------------------ //
+
+// input : str
+// output : str
+// objective : reverse the string using three differernt ways
+
+/*
+// method1 : using reverse for loop
+function reverse(str) {
+    let newStr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]
+    }
+
+    return newStr
+}
+// time : n
+// space : n
+*/
+
+/*
+function reverse(str) {
+    // method 2: recursion
+    if (str.length === 1) {
+        return str
+    }
+
+    return str[str.length - 1] + reverse(str.slice(0, str.length - 1))
+}
+// time : n^2
+// space : 1
+*/
+
+function reverse(str) {
+    // method: Using Array.prototype.reverse
+    return str.split('').reverse().join('')
+}
+// time: n
+// space: 1
 
 module.exports = reverse;
