515cb0ca62557a641d84dd387198ab48f0f7caaa
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-04 14:39:56 -0800

    Complete anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -14,11 +14,18 @@ function anagrams(stringA, stringB) {
     // objective : check if two strings have same count of each letter
     // method : use an object that collects the letters for each str
         // then compare both objects using JSON.stringify()
+    // pps : letters must be lower case
+         //  Get rid of anything that is not a letter
+         
     let strAObj = {}
     let currentLetter;
+    let filteredStringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    let filteredStringB = stringB.replace(/[^\w]/g, "").toLowerCase()
 
-    for (let i = 0; i < stringA.length; i++) {
-        currentLetter = stringA[i]
+    
+
+    for (let i = 0; i < filteredStringA.length; i++) {
+        currentLetter = filteredStringA[i]
         if (strAObj[currentLetter]) {
             strAObj[currentLetter] += 1
         } else {
@@ -27,8 +34,8 @@ function anagrams(stringA, stringB) {
     }
 
     let strBObj = {}
-    for (let j = 0; j < stringB.length; j++) {
-        currentLetter = stringB[j]
+    for (let j = 0; j < filteredStringB.length; j++) {
+        currentLetter = filteredStringB[j]
         if (strBObj[currentLetter]) {
             strBObj[currentLetter] += 1
         } else {
@@ -36,7 +43,19 @@ function anagrams(stringA, stringB) {
         }
     }
 
-    return JSON.stringify(strAObj) === JSON.stringify(strBObj)
+    return checkEqualObjects(strAObj, strBObj)
+}
+
+function checkEqualObjects(obj1, obj2) {
+    // input : obj, obj
+    // output : bool
+    // objective : check if the two objects are equal
+    // method : find longer length object by measuring length of keys and then check that the values
+    // for for each key are the same in both objects
+    // pps : Only works on one level objects (the value cannot be another object)
+    const longerObj = Object.keys(obj1).length > Object.keys(obj2) ? obj1 : obj2
+
+    return Object.keys(longerObj).every(key => obj1[key] === obj2[key]);
 }
 
 module.exports = anagrams;
