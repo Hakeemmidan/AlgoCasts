b06f07bc022cb2b597a992b9d60ece5a75c90543
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-04 19:02:04 -0800

    Complete steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,21 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    let result;
+
+    for (let i = 0; i < n; i++) {
+        result = '';
+        
+        for (let j = 0; j <= i; j++) {
+            result += '#'
+        }
+
+        for (let k = 0; k < n - 1 - i; k++) {
+            result += ' '
+        }
+        console.log(result)
+    }
+}
 
 module.exports = steps;
