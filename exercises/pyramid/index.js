21e9dad617b5056ac35b5625f1e5f139228139ea
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-06 16:50:50 -0800

    Complete pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -10,10 +10,32 @@
 //       ' # '
 //       '###'
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
+//       '  #  ' 5 - 1 === 1
+//       ' ### ' 5 - 2 === 3
 //       '#####'
 
-function pyramid(n) {}
+// input : int
+// output : str
+// objective : make a pyramid using hashtags (#) that has n number of rows
+
+function pyramid(n) {
+    const numCols = (n * 2) - 1
+    const midPoint = Math.floor(numCols / 2)
+    let res;
+
+    for (let row = 0; row < n; row++) {
+        res = '';
+
+        for (let col = 0; col < numCols; col++) {
+            if (midPoint - row <= col && midPoint + row >= col) {
+                res += '#'
+            } else {
+                res += ' '
+            }
+        }
+        
+        console.log(res)
+    }
+}
 
 module.exports = pyramid;
