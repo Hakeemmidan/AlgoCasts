c917ef7f6dccf18c74ec2c1fc0a1782d06cdda9b
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-07 20:43:31 -0800

    Fix pyramid recursive solution to not include any for loops

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -61,16 +61,23 @@ function pyramid(n, row = 0, level = '') {
     const numCols = (2 * n) - 1
     const midPoint = Math.floor(numCols / 2)
 
-    for (let col = 0; col < numCols; col++) {
-        if (midPoint - row <= col && midPoint + row >= col) {
-            level += '#'
-        } else {
-            level += ' '
-        }
+    // if the length of level is equal to number of columns
+    // then console log level
+    // go to next row
+    if (level.length === numCols) {
+        console.log(level)   
+        return pyramid(n, row + 1)
     }
-    console.log(level)
-    
-    return pyramid(n, row + 1)
+
+    let add;
+    // you can think of level as col
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        add = '#'
+    } else {
+        add = ' '
+    }
+
+    pyramid(n, row, level + add)
 }
 
 module.exports = pyramid;
