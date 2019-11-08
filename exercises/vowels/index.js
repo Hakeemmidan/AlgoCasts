ca060d6b7a73d02dce704a9723b0f715b1ba8e68
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-07 20:51:03 -0800

    Complete vowels iteratively

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// input : str
+// output : int
+// objective : count number of vowels in strings
+// pps : lower case string
+
+function vowels(str) {
+    // keep a counter set to 0
+    // have a variable that contins an array of vowels
+    // go through each letter using loop
+    // counter += 1 if char is a vowel
+    str = str.toLowerCase()
+    let counter = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            counter += 1
+        }
+    }
+
+    return counter
+}
 
 module.exports = vowels;
