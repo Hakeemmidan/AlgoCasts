54f9bf47cc2dc7a409f34887fce89a71034a2c5b
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-12 13:21:56 -0800

    Complete matrix problem

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+
+// input : int
+// output : 2D array
+// objective : create a spiral that has int rows
+function matrix(n) {
+    const results = [];
+
+    for (let i = 0; i < n; i++) {
+        results.push([])
+    }
+
+    let counter = 1;
+    let startCol = 0;
+    let startRow = 0;
+    let endCol = n - 1;
+    let endRow = n - 1;
+
+    while (startCol <= endCol && startRow <= endRow) {
+        // top row 
+        for (let i = startCol; i <= endCol; i++) {
+            results[startRow][i] = counter
+            counter++
+        }
+        startRow++
+
+        // right column
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endCol] = counter
+            counter++
+        }
+        endCol--
+
+        // bottom row
+        for (let i = endCol; i >= startCol; i--) {
+            results[endRow][i] = counter
+            counter++
+        }
+        endRow--
+
+        // left column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startCol] = counter
+            counter++
+        }
+        startCol++
+    }
+
+    return results
+}
 
 module.exports = matrix;
