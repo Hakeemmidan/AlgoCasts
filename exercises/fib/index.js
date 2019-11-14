a6c231afd18c317e759fa646a58563d36c10c5bd
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-14 14:52:28 -0800

    Solve fib iteratively with no arr

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,43 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// input : int
+// output : arr
+// objective : find the nth fib num
+
+// ---------------------------------------------------------------------------- //
+// method 1 : iterative with no arr
+function fib(n) {
+    // keep track of 4 variables
+    // last num, before last num, and counter (default is 2), and result
+    // if n === 0 return 0
+    // if n === 1 return 1
+    // Otherwise, add last num and before last num and increase counter by one
+        // Update other variables accordingly
+    // keep doing this until counter === n
+    // return result variable
+    
+    if (n === 0 || n === 1) {
+        return n;
+    }
+
+    let lastNum = 1;
+    let beforeLastNum = 0;
+    let result = lastNum + beforeLastNum;
+    let counter = 2;
+
+    for (counter; counter < n; counter++) {
+        beforeLastNum = lastNum;
+        lastNum = result;
+        result = beforeLastNum + lastNum;
+    }
+
+    return result;
+}
+// time : n
+// sapce : 1
+
+
 
 module.exports = fib;
