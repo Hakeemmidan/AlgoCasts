aaa3502f025576385dc9587f004464b570cbaf24
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-16 16:56:59 -0800

    Attempt then complete qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -13,7 +13,84 @@
 //     q.remove(); // returns 2
 
 const Stack = require('./stack');
+// queue fifo
+    // add => adds to top
+    // remove => removes from bottom
+// stack
+    // add => add to top
+    // remove => remove from top
 
-class Queue {}
+// input  : N/A
+// output : N/A
+// objective : create a queue class using two stacks
+
+class Queue {
+    // constructor
+        // stack A
+        // stack B
+        // the data is going to be contained within these two stacks
+    
+    // add
+        // unshift stack A
+    
+    // remove
+        // until stack A is empty (peek to see if there is anything there)
+            // shift  each item from stack A and unshfit it to stack B
+        // shift first item, save it in a variable
+        // until stack B is empty 
+            // shift each item from stack B and unshift it to stack B
+        // return saved var
+    
+    // peek
+        // do what you did in remove but instead of removing var, just peek into it, and save in var
+    
+    constructor() {
+        this.sA = new Stack();
+        this.sB = new Stack();
+    }
+
+    add(item) {
+        this.sA.push(item)
+    }
+
+    remove() {
+        // e.g.
+        // a : [1,2]
+        // b : []
+        
+        while (this.sA.peek()) {
+            this.sB.push(this.sA.pop())
+        }
+        // => b : [2,1]
+
+
+        let deletedItem = this.sB.pop();
+        // => deletedItem : 2
+        // => b : [1]
+
+        while (this.sB.peek()) {
+            this.sA.push(this.sB.pop())
+        }
+        // a : [1]
+        // b : []
+
+        return deletedItem;
+        // => 2
+    }
+
+    peek() {
+        while (this.sA.peek()) {
+            this.sB.push(this.sA.pop())
+        }
+
+        let peekedItem = this.sB.peek();
+
+        while (this.sB.peek()) {
+            this.sA.push(this.sB.pop())
+        }
+
+        return peekedItem;
+    }
+}
 
 module.exports = Queue;
