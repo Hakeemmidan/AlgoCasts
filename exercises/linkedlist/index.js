35fcd845b14a592c722c71eeb2bb69764c250874
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-19 19:16:01 -0800

    Complete for...of loop in linkedList using generator iterator

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -198,6 +198,14 @@ class LinkedList {
             node = node.next
         }
     }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node.next;
+        }
+    }
 }
 
 module.exports = { Node, LinkedList };
