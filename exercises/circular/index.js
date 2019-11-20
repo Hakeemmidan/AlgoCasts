37126994cfcbf4a7f1f638d25087c738acccb95d
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-19 19:55:09 -0800

    Attempt to solve circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,36 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+// input : linked list
+// output : bool
+// objective : Return true if linkedlist is circular
+// method : Iterate through linkedList
+    // keep list of visited nodes
+    // if the node that we are visiting next is in the
+    // list of visited nodes
+        // return true
+    // if you go through entire list
+        // return false
+
+function circular(list) {
+    // create visitedNode var
+    // create node var that starts at head
+    // go through nodes using for loop
+        // before entering each node, check if it's in visitedNodes
+        // if it is, return true
+    // return false
+    let visited = [];
+    let node = this.head;
+
+    while (node) {
+        if (visited.includes(node.next)) {
+            return true;
+        }
+        visited.push(node); 
+        node = node.next;
+    }
+
+    return false;
+}
 
 module.exports = circular;
