990424a2e833d5ba2d8d0f77e3a06dbff96259d3
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-19 20:09:29 -0800

    Complete circular using pointers

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,34 +12,17 @@
 //   c.next = b;
 //   circular(l) // true
 
-// input : linked list
-// output : bool
-// objective : Return true if linkedlist is circular
-// method : Iterate through linkedList
-    // keep list of visited nodes
-    // if the node that we are visiting next is in the
-    // list of visited nodes
-        // return true
-    // if you go through entire list
-        // return false
-
 function circular(list) {
-    // create visitedNode var
-    // create node var that starts at head
-    // go through nodes using for loop
-        // before entering each node, check if it's in visitedNodes
-        // if it is, return true
-    // return false
-    let visited = [];
-    let node = list.head;
+    let slow = list.head;
+    let fast = list.head;
 
-    while (node) {
-        if (visited.includes(node.next)) {
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (fast === slow) {
             return true;
         }
-
-        visited.push(node); 
-        node = node.next;
     }
 
     return false;
