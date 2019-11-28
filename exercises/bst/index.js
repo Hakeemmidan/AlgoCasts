06ec61356a644be9f41665629b35f70ec177c9f9
Hakeem Almidan <hakeemmidan@gmail.com>
2019-11-27 23:11:58 -0800

    Complete events

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -119,9 +119,7 @@ class Node {
 
         if (data === this.data) {
             return this;
-        }
-        
-        if (data < this.data && this.left) {
+        } else if (data < this.data && this.left) {
             return this.left.contains(data);
         } else if (data > this.data && this.right) {
             return this.right.contains(data);
